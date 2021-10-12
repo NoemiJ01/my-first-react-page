513569cf5b6249d13d87766076912cd13a52edb9
NoemiJ01 <89852801+NoemiJ01@users.noreply.github.com>
2021-10-12 12:28:15 -0400

    Changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -19,6 +19,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1> About Noemi </h1>
+      <p> I am 16 years old. </p>
+      <p> My birthday is on December 18th. </p>
+      <p> I like to dance and sing. </p>
+      <p> I attend DPCHS and I am a senior. </p>
     </div>
   );
 }
